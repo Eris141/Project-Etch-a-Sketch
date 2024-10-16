00830b7ad9e9b584d11f686c566f3b14e24e859f
Eris141 <165952691+Eris141@users.noreply.github.com>
2024-10-16 19:50:33 +0200

    feat: button to add grid number

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const gridContainer = document.querySelector('.container');
 
 // crate a loop and put 16 x 16 divs inside
-let gridSquare = 16;
 
-for (let i = 0; i < gridSquare; i++) {
-  for (let j = 0; j < gridSquare; j++) {
+function createGrid(gridSquare = 16) {
+  gridContainer.innerHTML = '';
+
+  for (let i = 0; i < gridSquare; i++) {
     let rows = document.createElement('div');
     rows.classList.add('rows');
+
+    const cellSise = 900 / gridSquare;// each cell will be 900/gid
+
+    for (let j = 0; j < gridSquare; j++) {
+      let border = document.createElement('div');
+      border.classList.add('border');
+      border.style.width = `${cellSise}px`;
+      border.style.height =  `${cellSise}px`
+      rows.appendChild(border);
+    }  
+
     gridContainer.appendChild(rows);
-    //addin color to each div
     rows.addEventListener('mouseover', (e) => {
-      e.target.classList.add('color');
+      if (e.target.classList.contains("border")) {
+        e.target.classList.add('color');
+      }
     })
   }
-}
+  }
+
+const divButton = document.createElement('div')
+const button = document.createElement("button");
+button.textContent = "Add Squares";
+button.classList.add("button");
+divButton.classList.add("btndiv");
+document.body.appendChild(divButton);
+divButton.appendChild(button);
+
+
+divButton.addEventListener('click', () => {
+  const squareNumber = prompt("Add a number for quare (from 1 to 100)");
+  
+
+  if (squareNumber === '' || isNaN(squareNumber) || squareNumber < 1 || squareNumber > 100) {
+    alert("please add a number between 1 and  100.");
+  } else {
+   createGrid(Number(squareNumber));
+  }
+})
+
+createGrid();
